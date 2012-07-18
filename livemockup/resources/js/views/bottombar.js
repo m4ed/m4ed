898362ecad8066400ba59c827786300b1da7fc1e
Joonas Ruuskanen <joonas.ruuskanen@aalto.fi>
2012-07-18 15:26:17 +0300

    Added more models and views and made everything twice as confusing

--- livemockup/resources/js/views/bottombar.js
+++ livemockup/resources/js/views/bottombar.js
@@ -15,37 +15,39 @@ function($, _, Backbone, AssetCollection, ImageView) {
             this.assets = new AssetCollection();
 
             this.assets.bind('reset', this.onReset, this);
+            this.assets.bind('change', this.render, this);
 
-            this.activeXhr = $.ajax({
-                url: '/api/media',
-                data: {},
-                type: 'GET',
+            this.assets.fetch({
                 error: function(jqXHR, textStatus, errorThrown) {
                     alert('WE FAILED!');
                 },
                 success: function(data, textStatus, jqXHR) {
                     self.assets.reset(data.images);
-                },
-                complete: function(jqHXR, textStatus) {
-                     self.activeXhr = null;
                 }
             });
         },
 
-        onReset: function() {
-            console.log(this.$el);
-            var self = this;
-            _.map(this.assets.models, function(model) {
-                var el = new ImageView({model: model}).render().el;
-                self.$el.append(el);
-                // Make the Textarea prettier with jQueryUI
-                this.$(el).draggable({
-                    revert: "invalid", // when not dropped, the item will revert back to its initial position
-                    containment: self.parent ? self.parent.el : "document", // stick to editor (el) if present
-                    helper: "clone",
-                    cursor: "move"
-                });
+        addOne: function() {
+            var view = new ImageView({model: model})
+              , el = view.render().el;
+            $(el).draggable({
+                revert: "invalid", // when not dropped, the item will revert back to its initial position
+                containment: this.parent ? this.parent.el : "document", // stick to editor (el) if present
+                helper: "clone",
+                cursor: "move"
             });
+            self.$el.append(el);
+        },
+
+        onReset: function() {
+            //console.log(this.$el);
+            //var self = this;
+            this.assets.each(this.addOne)
+            // _.map(this.assets.models, function(model) {
+            //     //
+                
+            //     // Make the Textarea prettier with jQueryUI
+            // });
         }
     });
 
